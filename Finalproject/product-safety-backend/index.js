02db437157c89a0d7ae5d4929631b1fda611782d
Srijan <srijandubey9452@gmail.com>
2025-06-13 05:16:15 +0530

    Fixed cors error

--- Finalproject/product-safety-backend/index.js
+++ Finalproject/product-safety-backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const cors = require('cors');
+
 const mongoose = require('mongoose');
 const { S3Client } = require('@aws-sdk/client-s3');
 const { Upload } = require('@aws-sdk/lib-storage');
